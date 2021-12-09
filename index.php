7ccca51ecca5114c75a32d3321003933bfb83d13
rota99C <rotaboolean@gmail.com>
2021-12-09 18:55:20 +0100

    mi sono bloccato perchè il server non rileva il file css, immagini. quindi non riesco a continuare senza vedere i risultati.

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php 
-require __DIR__ . '/layout/header.php';
+include __DIR__ . '/layout/header.php';
+
 
 ?>
 
 <div id="app">
-    
+<div class="container">
+    <div v-for="disco in dischi">
+
+    </div>
+</div>
+
+
 
 
 
