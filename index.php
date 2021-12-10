dbbe153cfcb3697215753f387ce5303b987331ac
rota99C <rotaboolean@gmail.com>
2021-12-10 17:28:22 +0100

    stilizzato sezione main e ciclato vari elementi presi da api

--- index.php
+++ index.php
@@ -5,10 +5,14 @@ require __DIR__ . '/layout/header.php';
 ?>
 
 <div id="app">
-<div class="container">
-    <div v-for="disco in dischi">
-
+<div class="container pt-5 d-flex flex-wrap justify-content-center">
+    <div class="singolo" v-for="disco in dischi">
+        <img :src="disco.poster" alt="">
+        <h4>{{disco.title}}</h4>
+        <p>{{disco.author}}</p>
+        <p>{{disco.year}}</p>
     </div>
+    
 </div>
 
 
